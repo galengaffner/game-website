8a77982eea2323d153aa7d21e9c147be3527b00f
Galen <galen@admin-users-MacBook-Pro-6.local>
2017-12-13 09:50:01 -0600

    First commit

---
+++ src/main/java/com/gngg/gamewebsite/GameWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.gngg.gamewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameWebsiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gngg/gamewebsite/GameWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gngg.gamewebsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
